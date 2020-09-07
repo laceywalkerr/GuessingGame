8b7e600a97a0d2a1f4ebda93bc127ec8eec51471
Lacey Walker <bluelarksong@gmail.com>
2020-09-07 18:20:53 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int MagicNumber = new Random().Next(1, 100);
+
+            Console.WriteLine("Welcome to the Guessing Game!");
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("I'm thinking of a number between 1 - 100, can you guess what it is?");
+
+            string guess = Console.ReadLine();
+            int UserNumber = int.Parse(guess);
+
+            Console.WriteLine("You guessed " + UserNumber);
+        }
+
+    }
+}
